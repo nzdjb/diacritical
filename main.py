2486ef48f5980c03cac4c71b2f0f50eb6c2c7463
nzdjb <github@2a1b.com>
2023-01-02 08:34:51 +0000

    feat: Initial commit.

---
+++ main.py
@@ -0,0 +1,32 @@
+from pywikibot import Site
+from re import findall, sub, IGNORECASE
+from tomllib import load
+from os import listdir, path
+
+configs = {}
+config_path = "config"
+for config_name in listdir(config_path):
+    with open(path.join(config_path, config_name), "rb") as f:
+        toml = load(f)
+        for name, config in toml.items():
+            configs[name] = config
+
+site = Site("en", "wikipedia")
+for name, config in configs.items():
+    print(f"{name}:")
+    i = 0
+    results = site.search(config["lookup_pattern"], namespaces=[0], content=True)
+    for result in results:
+        if str(result.title()) in config["ignored_pages"]:
+            continue
+        content = result.get()
+        for pattern in config["ignored_patterns"]:
+            content = sub(pattern, "", content, flags=IGNORECASE)
+        groups = findall(
+            ".{0,20}" + config["lookup_pattern"] + ".{0,20}", content, flags=IGNORECASE
+        )
+        if len(groups) > 0:
+            i = i + 1
+            print(f"{result.title()}: {result.full_url()}")
+            print(groups)
+    print(f"{i} articles with potential misspellings found.")
