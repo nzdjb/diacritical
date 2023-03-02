ff768ffa20dcdd6e89dd0e9ecf463ad3f77b44a5
nzdjb <github@2a1b.com>
2023-03-02 08:08:03 +0000

    chore: Types.

--- diacritical/search.py
+++ diacritical/search.py
@@ -1,14 +1,15 @@
 from unidecode import unidecode
 from pywikibot import Site
+from pywikibot.data.api import PageGenerator
 
 class Search:
     _namespaces=[0]
     
-    def __init__(self, site=None):
+    def __init__(self, site: Site=None):
         if not site:
             site = Site("en", "wikipedia")
         self._site = site
 
-    def search(self, lookup_pattern: str):
+    def search(self, lookup_pattern: str) -> PageGenerator:
         lookup_pattern = unidecode(lookup_pattern).lower()
         return self._site.search(lookup_pattern, namespaces=self._namespaces, content=True)
