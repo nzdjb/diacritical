55d147c3ff869b2f5899c24e35bbf820767c2000
nzdjb <github@2a1b.com>
2023-04-09 08:45:47 +0000

    docs: Types for config_parser.

--- diacritical/config_parser.py
+++ diacritical/config_parser.py
@@ -1,4 +1,5 @@
 from tomllib import load
+from typing import Self
 import os
 
 
@@ -6,14 +7,14 @@ class ConfigParser:
     def __init__(self):
         self.config = {}
 
-    def load_config(self, path):
+    def load_config(self, path: str) -> Self:
         with open(path, "rb") as f:
             toml = load(f)
             for key, value in toml.items():
                 self.config[key] = value
         return self
 
-    def load_config_dir(self, path):
+    def load_config_dir(self, path: str) -> Self:
         for config in os.listdir(path):
             self.load_config(os.path.join(path, config))
         return self
